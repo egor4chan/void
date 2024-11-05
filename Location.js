ab540888dcba7ac19e84085e50fb7d4eac6f691a
egor4chan <93943877+egor4chan@users.noreply.github.com>
2024-11-05 21:44:40 +0400

    Add files via upload

--- Location.js
+++ Location.js
@@ -1,3 +1,68 @@
+let LocationNow = 1;
+
+function check_location() {
+    if (LocationNow == 1) {
+        document.getElementById('content_void').style.top = '10vh'
+        document.getElementById('content_void').style.zIndex = '100';
+        document.getElementById('content_void').style.opacity = '1';
+
+        document.getElementById('content_tasks').style.top = '100vh'
+        document.getElementById('content_tasks').style.zIndex = '-100'
+        document.getElementById('content_tasks').style.opacity = '0'
+        document.getElementById('content_frens').style.top = '100vh'
+        document.getElementById('content_frens').style.zIndex = '-100'
+        document.getElementById('content_frens').style.opacity = '0'
+        document.getElementById('content_top').style.top = '100vh'
+        document.getElementById('content_top').style.zIndex = '-100'
+        document.getElementById('content_top').style.opacity = '0'
+    }
+    if (LocationNow == 2) {
+        document.getElementById('content_tasks').style.top = '10vh'
+        document.getElementById('content_tasks').style.zIndex = '100';
+        document.getElementById('content_tasks').style.opacity = '1';
+
+        document.getElementById('content_void').style.top = '100vh'
+        document.getElementById('content_void').style.zIndex = '-100'
+        document.getElementById('content_void').style.opacity = '0'
+        document.getElementById('content_frens').style.top = '100vh'
+        document.getElementById('content_frens').style.zIndex = '-100'
+        document.getElementById('content_frens').style.opacity = '0'
+        document.getElementById('content_top').style.top = '100vh'
+        document.getElementById('content_top').style.zIndex = '-100'
+        document.getElementById('content_top').style.opacity = '0'
+    }
+    if (LocationNow == 3) {
+        document.getElementById('content_frens').style.top = '10vh'
+        document.getElementById('content_frens').style.zIndex = '100';
+        document.getElementById('content_frens').style.opacity = '1';
+
+        document.getElementById('content_void').style.top = '100vh'
+        document.getElementById('content_void').style.zIndex = '-100'
+        document.getElementById('content_void').style.opacity = '0'
+        document.getElementById('content_tasks').style.top = '100vh'
+        document.getElementById('content_tasks').style.zIndex = '-100'
+        document.getElementById('content_tasks').style.opacity = '0'
+        document.getElementById('content_top').style.top = '100vh'
+        document.getElementById('content_top').style.zIndex = '-100'
+        document.getElementById('content_top').style.opacity = '0'
+    }
+    if (LocationNow == 4) {
+        document.getElementById('content_top').style.top = '10vh'
+        document.getElementById('content_top').style.zIndex = '100';
+        document.getElementById('content_top').style.opacity = '1';
+
+        document.getElementById('content_void').style.top = '100vh'
+        document.getElementById('content_void').style.zIndex = '-100'
+        document.getElementById('content_void').style.opacity = '0'
+        document.getElementById('content_tasks').style.top = '100vh'
+        document.getElementById('content_tasks').style.zIndex = '-100'
+        document.getElementById('content_tasks').style.opacity = '0'
+        document.getElementById('content_frens').style.top = '100vh'
+        document.getElementById('content_frens').style.zIndex = '-100'
+        document.getElementById('content_frens').style.opacity = '0'
+    }
+}
+
 function locate(src) {
     var WebApp = window.Telegram.WebApp;
     WebApp.HapticFeedback.impactOccurred('light')
@@ -7,23 +72,29 @@ function locate(src) {
         document.getElementById('page_2').style.opacity = 0.4;
         document.getElementById('page_3').style.opacity = 0.4;
         document.getElementById('page_4').style.opacity = 0.4;
+        LocationNow = 1;
     }
     if (src == 2) {
         document.getElementById('page_2').style.opacity = 1;
         document.getElementById('page_1').style.opacity = 0.4;
         document.getElementById('page_3').style.opacity = 0.4;
         document.getElementById('page_4').style.opacity = 0.4;
+        LocationNow = 2;
     }
     if (src == 3) {
         document.getElementById('page_3').style.opacity = 1;
         document.getElementById('page_2').style.opacity = 0.4;
         document.getElementById('page_1').style.opacity = 0.4;
         document.getElementById('page_4').style.opacity = 0.4;
+        LocationNow = 3;
     }
     if (src == 4) {
         document.getElementById('page_4').style.opacity = 1;
         document.getElementById('page_2').style.opacity = 0.4;
         document.getElementById('page_3').style.opacity = 0.4;
         document.getElementById('page_1').style.opacity = 0.4;
+        LocationNow = 4;
     }
+
+    check_location()
 }
