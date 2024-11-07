844fb3c0548e156ad199cb40aaaaed9ef55c715c
egor4chan <93943877+egor4chan@users.noreply.github.com>
2024-11-07 21:50:35 +0400

    Add files via upload

--- StartWindow.js
+++ StartWindow.js
@@ -1,3 +1,10 @@
+var WebApp = window.Telegram.WebApp;
+WebApp.expand()
+
+WebApp.headerColor = '#f7f7f7'
+WebApp.backgroundColor = '#f7f7f7';
+WebApp.bottomBarColor = '#f7f7f7'
+
 setTimeout(() => {
     window.location.href = 'index.html'
 }, 3000)
