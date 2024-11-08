ef70efb8a2ee46e05085f3c99300e0a48d85fe27
egor4chan <93943877+egor4chan@users.noreply.github.com>
2024-11-08 18:44:58 +0400

    Add files via upload

--- Telegram.js
+++ Telegram.js
@@ -2,7 +2,7 @@ var WebApp = window.Telegram.WebApp;
 var username = WebApp.initDataUnsafe.user.username;
 var userPhoto = WebApp.initDataUnsafe.user.photo_url;
 
-
+alert(`photo: ${userPhoto}`)
 
 if (username == undefined) 
     username = 'noname'
