1b3ddf46be6986d71f8d80b2a9f1db8e5c8515f5
egor4chan <93943877+egor4chan@users.noreply.github.com>
2024-11-09 15:53:15 +0400

    Add files via upload

--- Basic.js
+++ Basic.js
@@ -4,3 +4,15 @@ WebApp.expand()
 WebApp.headerColor = '#f7f7f7'
 WebApp.backgroundColor = '#f7f7f7';
 WebApp.bottomBarColor = '#f7f7f7'
+
+function remove_dr() {
+    document.getElementById('dailyreward').style.bottom = '-400px';
+    document.getElementById('content_void').style.filter = 'blur(0px)'
+}
+
+function daily_reward() {
+    document.getElementById('dailyreward').style.bottom = '0px';
+    document.getElementById('content_void').style.filter = 'blur(20px)'
+}
+
+daily_reward()
--- Game.js
+++ Game.js
@@ -1,3 +1,11 @@
 var void_cloud = document.getElementById('content_void');
 
-void_cloud.style.opacity = '0'
+function open_game(game) {
+    document.getElementById('content_void').style.top = '30vh'
+    document.getElementById('content_void').style.zIndex = '-100'
+    document.getElementById('content_void').style.opacity = '0'
+
+    document.getElementById('game_hold').style.top = '10vh'
+    document.getElementById('game_hold').style.zIndex = '100';
+    document.getElementById('game_hold').style.opacity = '1';
+}
